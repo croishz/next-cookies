15815bf401c42b2dae076436573dfcc1b8f332ff
lesh <email@email.com>
2023-12-30 13:33:41 +0900

    add test route handler

---
+++ src/app/api/route.ts
@@ -0,0 +1,25 @@
+/** @format */
+
+import { NextRequest, NextResponse } from "next/server";
+import { cookies } from "next/headers";
+
+export async function GET(request: NextRequest) {
+	const key_name = "token";
+	cookies().set(key_name, "test");
+
+	const response = NextResponse.json(
+		{
+			msg: "Hell world!",
+			key_name,
+		},
+		{
+			status: 200,
+		}
+	);
+	response.cookies.set(key_name, "override");
+
+	console.log("cookie from request(from document)", request.cookies.get(key_name));
+	console.log("cookie", cookies().get(key_name));
+	console.log("cookie from response", response.cookies.get(key_name));
+	return response;
+}
